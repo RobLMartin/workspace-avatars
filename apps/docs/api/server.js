4bc28dc267bfecf0fbcda29a46032b8b7d8efb7d
Rob Martin <rmartin3@alumni.unca.edu>
2026-04-27 14:48:12 -0400

    rr7 config in vercel.json

---
+++ apps/docs/api/server.js
@@ -0,0 +1,5 @@
+// Vercel serverless function for React Router v7
+import { createRequestHandler } from "@react-router/node";
+import * as build from "../build/server/index.js";
+
+export default createRequestHandler({ build, mode: process.env.NODE_ENV });
